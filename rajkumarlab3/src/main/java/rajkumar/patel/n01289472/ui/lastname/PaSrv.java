fb5ffb3b4de707c22c89126e81ad8a864a5ab567
RajkumarPatel28 <n01289472@humbermail.ca>
2020-12-02 15:53:23 -0500

    PaSrv Activity
    - Added edittext and a button linked to eachother
    - Added error messages for user if the zip code is not correctly enter

--- rajkumarlab3/src/main/java/rajkumar/patel/n01289472/ui/lastname/PaSrv.java
+++ rajkumarlab3/src/main/java/rajkumar/patel/n01289472/ui/lastname/PaSrv.java
@@ -1,9 +1,12 @@
 package rajkumar.patel.n01289472.ui.lastname;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,9 +18,34 @@ import rajkumar.patel.n01289472.R;
 
 public class PaSrv extends Fragment {
 
+    EditText rajZipEt;
+    Button rajWebBtn;
+    String zipCode;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.pa_srv, container, false);
+
+        rajZipEt = view.findViewById(R.id.rajZipEt);
+
+        rajWebBtn = view.findViewById(R.id.rajWebBtn);
+        rajWebBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                zipCode = rajZipEt.getText().toString().trim();
+
+                if (TextUtils.isEmpty(zipCode)){
+                    rajZipEt.setError(getString(R.string.error_message));
+                    return;
+                }
+
+                if (zipCode.length() > 5) {
+                    rajZipEt.setError(getString(R.string.error_message2));
+                    return;
+                }
+            }
+        });
 
         return view;
     }
