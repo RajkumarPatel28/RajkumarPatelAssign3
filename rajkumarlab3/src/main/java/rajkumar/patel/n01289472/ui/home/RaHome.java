7f16ac46d3ae5fd13fc81f08d5fe520ac9599cbf
RajkumarPatel28 <n01289472@humbermail.ca>
2020-12-02 02:22:06 -0500

    RaHome Fragment
    - Added textviews for name and id
    - Added current time and date
    - Added the Home Fragment as an item on the navigation drawer

--- rajkumarlab3/src/main/java/rajkumar/patel/n01289472/ui/home/RaHome.java
+++ rajkumarlab3/src/main/java/rajkumar/patel/n01289472/ui/home/RaHome.java
@@ -1,11 +1,17 @@
 package rajkumar.patel.n01289472.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 import rajkumar.patel.n01289472.R;
 
@@ -15,9 +21,16 @@ import rajkumar.patel.n01289472.R;
 
 public class RaHome extends Fragment {
 
+    TextView rajDate;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.ra_home, container, false);
+
+        Calendar date = Calendar.getInstance();
+        String currentdate = DateFormat.getDateInstance(DateFormat.FULL).format(date.getTime());
+        rajDate = view.findViewById(R.id.rajDate);
+        rajDate.setText(currentdate);
 
         return view;
     }
